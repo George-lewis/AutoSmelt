25af308714a76d969204705d4736d8fbd67d54fa
George Lewis <georgejeffreylewis@gmail.com>
2020-08-16 14:56:38 -0400

    Make enchantment require more exp, more rare, incompatble with silk touch

--- src/main/java/com/glewis/autosmelt/SmeltingEnchantment.java
+++ src/main/java/com/glewis/autosmelt/SmeltingEnchantment.java
@@ -2,6 +2,7 @@ package com.glewis.autosmelt;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.EquipmentSlot;
 
 /**
@@ -10,7 +11,7 @@ import net.minecraft.entity.EquipmentSlot;
 public class SmeltingEnchantment extends Enchantment {
 
 	protected SmeltingEnchantment() {
-		super(Rarity.RARE, EnchantmentTarget.DIGGER, new EquipmentSlot[] { EquipmentSlot.MAINHAND });
+		super(Rarity.VERY_RARE, EnchantmentTarget.DIGGER, new EquipmentSlot[] { EquipmentSlot.MAINHAND });
     }
 
     @Override
@@ -21,6 +22,21 @@ public class SmeltingEnchantment extends Enchantment {
     @Override
     public int getMaxLevel() {
         return 1;
+    }
+
+    @Override
+    public int getMinPower(int level) {
+        return 15;
+    }
+
+    @Override
+    public int getMaxPower(int level) {
+        return super.getMinPower(level) + 50;
+    }
+
+    @Override
+    protected boolean canAccept(Enchantment other) {
+        return super.canAccept(other) && !other.equals(Enchantments.SILK_TOUCH);
     }
     
 }
